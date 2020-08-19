67e4b1462ff6b7074fc63bdfc112d5d25cddda7a
Zack <ZJonas1995@gmail.com>
2020-08-19 18:51:03 -0400

    Created method to parse a string and return the longest word + it's length

--- LongestWord/LongestWord/Program.cs
+++ LongestWord/LongestWord/Program.cs
@@ -15,5 +15,27 @@ namespace LongestWord
 			get;
 		}
 
+		class Program
+		{
+			static WordInfo LongestWord(string str)
+			{
+				WordInfo longestWordInfo = new WordInfo();
+				string[] strSplit = str.Split(" ");
+
+				longestWordInfo.WordName = strSplit[0];
+				longestWordInfo.WordLength = strSplit[0].Length;
+
+
+				foreach (string s in strSplit)
+				{
+					if (s.Length > longestWordInfo.WordLength)
+					{
+						longestWordInfo.WordName = s;
+						longestWordInfo.WordLength = s.Length;
+					}
+				}
+				return longestWordInfo;
+			}
+		}
 	}
 }
