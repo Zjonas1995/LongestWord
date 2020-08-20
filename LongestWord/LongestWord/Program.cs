88ffca9daa22cd8452f25944b206136177a1e79c
Zack <ZJonas1995@gmail.com>
2020-08-19 21:10:37 -0400

    Added assumptions in the comments

--- LongestWord/LongestWord/Program.cs
+++ LongestWord/LongestWord/Program.cs
@@ -25,6 +25,16 @@ namespace LongestWord
 				Console.WriteLine(String.Format("Longest word in the string: \"{0}\" Length of the word: {1}", testWordInfo.WordName, testWordInfo.WordLength));
 			}
 
+			/* Assumptions
+			 * - The he prompt wanted this method to return both the string and the length, so I decided to utilize a WordInfo object. In most similar cases
+			 *   I would just return a string and then call string.Length where the length is needed.
+			 * - If passed a null string, I will return null. The assumption is that null further null handling will occur wherever the method is called if
+			 *   it is needed. (An alternative could be to return an empty string with length 0 in the null case).
+			 * - A sentence with two equal length longest words will return the earlier occurence. This can be easily adjusted by changing the logic to search for
+			 *   >= instead of >
+			 * - By this method, a word is anything other than a space. An additional method was made that splits by other characters for proof of concept, if that
+			 *   was the intention, but the assumption I am working with is that a word is any character, seperated by a space.
+			 */
 			public static WordInfo LongestWord(string str)
 			{
 				WordInfo longestWordInfo = new WordInfo();
@@ -52,6 +62,10 @@ namespace LongestWord
 				return longestWordInfo;
 			}
 
+			/* Assumptions
+			 * - Right now this method splits words by anything that isn't a space, period, comma, colon, semi-colon, !, and ?. This can be expanded on easily
+			 *   if a more specific spec of what is or isn't a word is provided. 
+			 */
 			public static WordInfo LongestWordRegex(string str)
 			{
 				WordInfo longestWordInfo = new WordInfo();
